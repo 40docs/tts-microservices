9b673393de5eef992c3ec9d07c0f9ef0f237766c
AJLab-GH <ajammes.ftnt@gmail.com>
2025-03-31 14:38:56 -0600

    changing from mono-to-stereo

--- azure_tts_service.py
+++ azure_tts_service.py
@@ -9,8 +9,9 @@ def synthesize(text: str, filename: str = "output") -> str:
     speech_config = speechsdk.SpeechConfig(subscription=SPEECH_KEY, region=SPEECH_REGION)
     speech_config.speech_synthesis_voice_name = VOICE
     speech_config.set_speech_synthesis_output_format(
-        speechsdk.SpeechSynthesisOutputFormat.Audio48Khz192KBitRateMonoMp3
+        speechsdk.SpeechSynthesisOutputFormat.Audio48Khz192KBitRateStereoMp3
     )
+
     audio_config = speechsdk.audio.AudioOutputConfig(filename=str(audio_path))
     synthesizer = speechsdk.SpeechSynthesizer(speech_config, audio_config)
 
